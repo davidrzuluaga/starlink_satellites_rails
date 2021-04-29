fc516b68486bc478a05f819c6fd3f46c53d7ed5e
David R Zuluaga <david@davidrzuluaga.com>
2021-04-29 12:03:16 -0500

    add getDistanceInMeters and addDistanceFromCoordinates methods

--- app/controllers/satellite_trackers_controller.rb
+++ app/controllers/satellite_trackers_controller.rb
@@ -1,13 +1,21 @@
 require 'uri'
 require 'net/http'
+require 'json'
+require 'haversine'
 
 class SatelliteTrackersController < ApplicationController
     def index
+        originLatitude = params[:originLatitude]
+        originLongitude = params[:originLongitude]
+        number = params[:number]
+
         satellites = callSpaceXSatellites()
         
         satellitesWithLocation = filterByLocationNotNULL(satellites)
         
-        render json: satellitesWithLocation
+        satWithDistance = addDistanceFromCoordinates(satellitesWithLocation, originLatitude.to_i, originLongitude.to_i)
+
+        render json: satWithDistance
     end
 
 private
@@ -20,5 +28,21 @@ private
 
     def filterByLocationNotNULL(satellites)
         return satellites.select {|satellite| satellite["longitude"] } 
+    end
+
+    def getDistanceInMeters(firstLat, firstLon, secondLat, secondLon)
+        distance = Haversine.distance(firstLat, firstLon, secondLat, secondLon)
+        return distance.to_meters
+    end
+
+    def addDistanceFromCoordinates(satellites, originLatitude, originLongitude)
+        arrayWithDistance = []
+        satellites.each do |satellite|
+            satLatitude = satellite["latitude"]
+            satLongitude = satellite["longitude"]
+            satellite["distanceWithOrigin"] = getDistanceInMeters(satLatitude, satLongitude, originLatitude, originLongitude)
+            arrayWithDistance.push(satellite)
+        end
+        return arrayWithDistance
     end
 end
