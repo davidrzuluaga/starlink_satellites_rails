44bd33e7301ca05e0eac503a19942617f2871ee4
David R Zuluaga <david@davidrzuluaga.com>
2021-04-29 11:31:39 -0500

    callSpaceXSatellites method

--- app/controllers/satellite_trackers_controller.rb
+++ app/controllers/satellite_trackers_controller.rb
@@ -1,3 +1,17 @@
-class SatelliteTrackersController < ApplicationController
+require 'uri'
+require 'net/http'
 
+class SatelliteTrackersController < ApplicationController
+    def index
+        satellites = callSpaceXSatellites()
+        render json: satellites
+    end
+    
+    private
+    def callSpaceXSatellites
+        uri = URI('https://api.spacexdata.com/v4/starlink')
+        res = Net::HTTP.get_response(uri)
+        body = res.body if res.is_a?(Net::HTTPSuccess)
+        return JSON.parse(body, object_class: OpenStruct) 
+    end
 end
